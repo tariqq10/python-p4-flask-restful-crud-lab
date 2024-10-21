789634c8e0bfc5d01b5cb5834bea0a1c86a9ac86
tariqq10 <Kwilliam866@gmail.com>
2024-10-21 13:00:56 +0300

    final

--- server/models.py
+++ server/models.py
@@ -7,10 +7,10 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String, nullable=False)  # Consider making this required
+    image = db.Column(db.String, nullable=False)  # Consider making this required
+    price = db.Column(db.Float, nullable=False)  # Consider making this required
+    is_in_stock = db.Column(db.Boolean, default=True)  # Default value added
 
     def __repr__(self):
         return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
